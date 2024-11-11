c565b55c410648a3a094e9421b215ba440cee1d0
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-11-11 23:37:46 +0900

    chore/#3: 아이콘 및 이미지 추가

---
+++ src/assets/LoginPage/index.js
@@ -0,0 +1,4 @@
+export { default as BackgroundImg } from './img_background.svg'
+export { ReactComponent as Title } from './img_title.svg'
+export { default as Email } from './ic_email.svg'
+export { default as Password } from './ic_password.svg'
