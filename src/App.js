aa0a7634d4a075174a5c9cd76fcc9f5c8f07532c
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-11-14 23:29:46 +0900

    design/#4: 메인페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css'
 import { Route, Routes } from 'react-router'
-import Login from './pages/Login/Login'
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import { Login, Main } from './pages'
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false)
+
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<div>Main</div>} />
+      <Route
+        path="/login"
+        element={
+          isLogin ? (
+            <Navigate to="/" replace />
+          ) : (
+            <Login setIsLogin={setIsLogin} />
+          )
+        }
+      />
+      <Route path="/" element={<Main setIsLogin={setIsLogin} />} />
+      <Route path="tutorlist" element={<Main setIsLogin={setIsLogin} />} />
     </Routes>
   )
 }
---
+++ src/pages/Main/Main.style.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100dvh - 2.8em);
+  min-height: fit-content;
+  width: calc(100dvw - 4em);
+  min-height: calc(100dvh - 2.8em);
+  min-width: calc(100dvw - 2.4em);
+  background-color: #fffef8;
+  padding: 1.4em 1.2em;
+
+  min-width: max-content;
+  min-height: max-content;
+`
+export const MainMenuContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+export const RecommendButton = styled.button`
+  border: none;
+  border-radius: 10em;
+  background-color: #ffeb89;
+  font-size: 1.1em;
+  font-weight: 700;
+  cursor: pointer;
+  outline: none;
+  padding: 0.9em 1.2em 0.8em 1.2em;
+  height: 3em;
+  &:hover {
+    background-color: #ffe066;
+  }
+  &:active {
+    background-color: #ffd633;
+  }
+`
+export const Title = styled.div`
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 0.3em;
+`
+export const ContentContainer = styled.div`
+  display: flex;
+  height: 48%;
+  width: 100%;
+  margin: 1.5em 0;
+`
+export const Content = styled.div`
+  flex: 1;
+  height: calc(100% - 2em);
+  margin: 0 1.5em;
+  display: flex;
+  flex-direction: column;
+`
