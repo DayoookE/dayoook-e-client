e76ba4eaf8c7f2fcd90221900b936ff128f65b07
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-12-03 15:07:48 +0900

    chore/#5: 아이콘 및 이미지 추가

---
+++ src/assets/ModalComponent/index.js
@@ -0,0 +1,4 @@
+export { default as XIcon } from './ic_x.svg'
+export { default as DayookIcon } from './ic_dayook.svg'
+export { default as PrevIcon } from './ic_prev.svg'
+export { default as NextIcon } from './ic_next.svg'
