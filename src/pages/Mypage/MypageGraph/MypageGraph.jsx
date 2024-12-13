37652bc9069eaad66b7678c2f9d7bbb49c1077ca
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-12-14 05:06:29 +0900

    feat/#48: 수강 신청 승인 API 연동

--- src/pages/Mypage/MypageGraph/MypageGraph.jsx
+++ src/pages/Mypage/MypageGraph/MypageGraph.jsx
@@ -1,59 +1,60 @@
-import { useState, useEffect } from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import Statistics from '../Statistics/Statistics';
-import 'react-circular-progressbar/dist/styles.css';
-import * as s from './MypageGraph.style';
+import { useState, useEffect } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import Statistics from '../Statistics/Statistics'
+import 'react-circular-progressbar/dist/styles.css'
+import * as s from './MypageGraph.style'
+import TuteeApplication from '../TutorMypage/TuteeApplication/TuteeApplication'
 
 const ProgressProvider = ({ valueStart, valueEnd, children }) => {
-  const [value, setValue] = useState(valueStart);
+  const [value, setValue] = useState(valueStart)
   useEffect(() => {
-    setValue(valueEnd);
-  }, [valueEnd]);
+    setValue(valueEnd)
+  }, [valueEnd])
 
-  return children(value);
-};
+  return children(value)
+}
 
-export default function MypageGraph({ isTutor, userInfo }) {
-  const graphContent = getGraphContent(userInfo); // 동적으로 그래프 내용 설정
+export default function MypageGraph({ isTutor, userInfo, userId }) {
+  const graphContent = getGraphContent(userInfo) // 동적으로 그래프 내용 설정
 
   return (
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <s.GraphWrapper>
-          {/* 그래프 */}
-          {(isTutor ? tutorGraphContent : graphContent).map((content, index) => (
-              <s.GraphContainer key={index}>
-                {/* 원형 그래프 */}
-                <ProgressProvider valueStart={0} valueEnd={content.percent}>
-                  {(value) => (
-                      <CircularProgressbar
-                          value={value}
-                          strokeWidth={30}
-                          styles={buildStyles({
-                            pathColor: content.pathColor,
-                            trailColor: content.trailColor,
-                            strokeLinecap: 'butt',
-                          })}
-                      />
-                  )}
-                </ProgressProvider>
-                {/* 그래프 텍스트 */}
-                <s.GraphTextWrapper>
-                  <div>{content.percent}%</div>
-                  <div>{content.title}</div>
-                </s.GraphTextWrapper>
-              </s.GraphContainer>
-          ))}
-        </s.GraphWrapper>
-        {/* 학습 분석 */}
-        {isTutor ? (
-            <div>list</div>
-        ) : (
-            <s.StatisticsContainer>
-              <Statistics />
-            </s.StatisticsContainer>
-        )}
-      </div>
-  );
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <s.GraphWrapper>
+        {/* 그래프 */}
+        {(isTutor ? tutorGraphContent : graphContent).map((content, index) => (
+          <s.GraphContainer key={index}>
+            {/* 원형 그래프 */}
+            <ProgressProvider valueStart={0} valueEnd={content.percent}>
+              {(value) => (
+                <CircularProgressbar
+                  value={value}
+                  strokeWidth={30}
+                  styles={buildStyles({
+                    pathColor: content.pathColor,
+                    trailColor: content.trailColor,
+                    strokeLinecap: 'butt',
+                  })}
+                />
+              )}
+            </ProgressProvider>
+            {/* 그래프 텍스트 */}
+            <s.GraphTextWrapper>
+              <div>{content.percent}%</div>
+              <div>{content.title}</div>
+            </s.GraphTextWrapper>
+          </s.GraphContainer>
+        ))}
+      </s.GraphWrapper>
+      {/* 학습 분석 */}
+      {isTutor ? (
+        <TuteeApplication userId={userId} />
+      ) : (
+        <s.StatisticsContainer>
+          <Statistics />
+        </s.StatisticsContainer>
+      )}
+    </div>
+  )
 }
 
 // 그래프 내용을 동적으로 반환하는 함수
@@ -70,7 +71,7 @@ const getGraphContent = (userInfo) => [
     pathColor: '#FF8484',
     trailColor: '#FFDBDB',
   },
-];
+]
 
 const tutorGraphContent = [
   {
@@ -85,4 +86,4 @@ const tutorGraphContent = [
     pathColor: '#FF8484',
     trailColor: '#FFDBDB',
   },
-];
+]
