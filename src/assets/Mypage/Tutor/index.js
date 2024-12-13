0833e08186d76446b7ed3a03d8988498537b1510
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-12-13 15:35:49 +0900

    chore/#17: 아이콘 추가

---
+++ src/assets/Mypage/Tutor/index.js
@@ -0,0 +1,2 @@
+export { default as EnterClassIcon } from './ic_class_enter.svg'
+export { default as ExitClassIcon } from './ic_class_exit.svg'
--- src/assets/level/index.js
+++ src/assets/level/index.js
@@ -1,1 +1,4 @@
 export { default as SproutIcon } from './ic_ sprout.svg'
+export { default as FlowerIcon } from './ic_flower.svg'
+export { default as FruitIcon } from './ic_fruit.svg'
+export { default as LeafIcon } from './ic_leaf.svg'
