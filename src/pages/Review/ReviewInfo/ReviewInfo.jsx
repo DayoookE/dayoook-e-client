a7bb2b780019e9b11ae54a2dca57ac2037bbd769
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-12-14 01:08:22 +0900

    feat/#39: 복습 리스트 조회

--- src/pages/Review/ReviewInfo/ReviewInfo.jsx
+++ src/pages/Review/ReviewInfo/ReviewInfo.jsx
@@ -1,14 +1,73 @@
-import { AwardIcon } from '../../../assets/Mypage/Review'
+import { useState, useEffect } from 'react'
+import {
+  AwardIcon,
+  ListDownIcon,
+  ListUpIcon,
+} from '../../../assets/Mypage/Review'
+import axios from 'axios'
 import * as s from './ReviewInfo.style'
 
 export default function ReviewInfo() {
+  const [isReviewListOpen, setIsReviewListOpen] = useState(false)
+  const [reviewList, setReviewList] = useState([])
+
+  useEffect(() => {
+    // 복습 리스트 api 연동
+    const fetchReviewList = async () => {
+      try {
+        const token = localStorage.getItem('dayookeAccessToken')
+        if (!token) {
+          return
+        }
+
+        const response = await axios.get(
+          `${process.env.REACT_APP_FAST_API_URL}/review`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        console.log(response.data.lesson_schedules)
+        setReviewList(response.data.lesson_schedules)
+      } catch (error) {
+        console.error('복습 리스트 조회 실패:', error)
+      }
+    }
+    fetchReviewList()
+  }, [])
+
   return (
     <s.ReviewInfoContainer>
       {/* 복습 주차 */}
       <s.TitleContainer>
-        <div>1주차 복습</div>
-        <div>2024년 11월 6일 수요일</div>
+        <div>
+          <div>1주차 복습</div>
+          <div>2024년 11월 6일 수요일</div>
+        </div>
+        <img
+          src={isReviewListOpen ? ListUpIcon : ListDownIcon}
+          alt="list"
+          onClick={() => setIsReviewListOpen(!isReviewListOpen)}
+        />
       </s.TitleContainer>
+
+      {isReviewListOpen && (
+        <s.ReviewListPosition>
+          <s.ReviewListContainer>
+            {reviewList.length > 0 ? (
+              reviewList.map((review, index) => (
+                <s.ReviewList key={index}>
+                  <div>✨&nbsp;&nbsp;{review.week}주차</div>
+                  <div>{review.date}</div>
+                </s.ReviewList>
+              ))
+            ) : (
+              <s.EmptyContent>
+                😢&nbsp;&nbsp;복습 내용이 없습니다
+              </s.EmptyContent>
+            )}
+          </s.ReviewListContainer>
+        </s.ReviewListPosition>
+      )}
 
       {/* 획득 포인트 */}
       <s.PointContainer>
