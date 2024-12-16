d24e2ac8bd1708618fbcb4826f18a6ab4bb28c62
wnsdyd1128 <mcleroysane19@inha.edu>
2024-12-16 20:17:36 +0900

    feat/#58 튜터 필터링 기능 추가

--- src/components/common/MappingContext.jsx
+++ src/components/common/MappingContext.jsx
@@ -1,29 +1,32 @@
 // contexts/MappingContext.jsx
-import { createContext, useContext, useState, useEffect } from 'react';
+import {createContext, useContext, useState, useEffect} from 'react';
 import axios from 'axios';
 
 const MappingContext = createContext();
 
-export const MappingProvider = ({ children }) => {
+export const MappingProvider = ({children}) => {
     const [timeSlots, setTimeSlots] = useState([]);
     const [languages, setLanguages] = useState([]);
     const [days, setDays] = useState([]);
     const [countries, setCountries] = useState([]);
+    const [ageGroups, setAgeGroups] = useState([]);
 
     useEffect(() => {
         const fetchMappingData = async () => {
             try {
-                const [timeSlotsRes, languagesRes, daysRes, countriesRes] = await Promise.all([
+                const [timeSlotsRes, languagesRes, daysRes, countriesRes, ageGroupsRes] = await Promise.all([
                     axios.get(`${process.env.REACT_APP_SPRING_API_URL}/timeSlots`),
                     axios.get(`${process.env.REACT_APP_SPRING_API_URL}/languages`),
                     axios.get(`${process.env.REACT_APP_SPRING_API_URL}/days`),
-                    axios.get(`${process.env.REACT_APP_SPRING_API_URL}/countries`)
+                    axios.get(`${process.env.REACT_APP_SPRING_API_URL}/countries`),
+                    axios.get(`${process.env.REACT_APP_SPRING_API_URL}/ageGroups`),
                 ]);
 
                 setTimeSlots(timeSlotsRes.data.result);
                 setLanguages(languagesRes.data.result);
                 setDays(daysRes.data.result);
                 setCountries(countriesRes.data.result);
+                setAgeGroups(ageGroupsRes.data.result);
             } catch (error) {
                 console.error('Error fetching mapping data:', error);
             }
@@ -38,7 +41,8 @@ export const MappingProvider = ({ children }) => {
                 timeSlots,
                 languages,
                 days,
-                countries
+                countries,
+                ageGroups
             }}
         >
             {children}
--- src/pages/Tutor/TutorMenu/TutorMenu.jsx
+++ src/pages/Tutor/TutorMenu/TutorMenu.jsx
@@ -1,38 +1,39 @@
-import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useRef, useState} from 'react'
 import ModalComponent from '../../../components/ModalComponent/ModalComponent'
-import { MenuBar } from '../../../components'
-import { SearchIcon } from '../../../assets/TutorPage'
+import {MenuBar} from '../../../components'
+import {SearchIcon} from '../../../assets/TutorPage'
 import * as ms from '../../Main/Main.style'
 import * as s from './TutorMenu.style'
+import axios from "axios";
 
-export default function TutorMenu({ setIsRecommend }) {
-  const navigate = useNavigate()
-  const [modalIsOpen, setIsOpen] = useState(false)
+export default function TutorMenu({setIsRecommend, searchKeywordRef, handleSearch}) {
+    const navigate = useNavigate()
+    const [modalIsOpen, setIsOpen] = useState(false)
 
-  const handleSubmit = () => {
-    setIsOpen(false)
-    setIsRecommend(true)
-    navigate('/tutorlist')
-  }
+    const handleSubmit = () => {
+        setIsOpen(false)
+        setIsRecommend(true)
+        navigate('/tutorlist')
+    }
 
-  return (
-    <ms.MainMenuContainer>
-      <s.MenuLeftContainer>
-        <s.SearchContainer>
-          <s.SearchInput placeholder="튜터를 검색해보세요  🙌" />
-          <s.SearchBtn src={SearchIcon} alt="Search" />
-        </s.SearchContainer>
-        <ms.RecommendButton onClick={() => setIsOpen(true)}>
-          🌱 다육이에게 튜터 추천받기
-        </ms.RecommendButton>
-        <ModalComponent
-          handleSubmit={handleSubmit}
-          modalIsOpen={modalIsOpen}
-          setIsOpen={setIsOpen}
-        />
-      </s.MenuLeftContainer>
-      <MenuBar />
-    </ms.MainMenuContainer>
-  )
+    return (
+        <ms.MainMenuContainer>
+            <s.MenuLeftContainer>
+                <s.SearchContainer>
+                    <s.SearchInput placeholder="튜터를 검색해보세요  🙌" ref={searchKeywordRef}/>
+                    <s.SearchBtn src={SearchIcon} alt="Search" onClick={handleSearch}/>
+                </s.SearchContainer>
+                <ms.RecommendButton onClick={() => setIsOpen(true)}>
+                    🌱 다육이에게 튜터 추천받기
+                </ms.RecommendButton>
+                <ModalComponent
+                    handleSubmit={handleSubmit}
+                    modalIsOpen={modalIsOpen}
+                    setIsOpen={setIsOpen}
+                />
+            </s.MenuLeftContainer>
+            <MenuBar/>
+        </ms.MainMenuContainer>
+    )
 }
