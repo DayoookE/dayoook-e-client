d13ae08ec4683a167bbdfa4f095d34e7afd612ed
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-12-17 03:00:22 +0900

    design/#86: 동화 좋아요 이미지 크기 수정

--- src/pages/Study/StudyContent/StudyOption/StudyOption.jsx
+++ src/pages/Study/StudyContent/StudyOption/StudyOption.jsx
@@ -1,40 +1,48 @@
-import {HeartIcon, SearchIcon} from '../../../../assets/Study'
+import { BigWhiteHeart, HeartIcon, SearchIcon } from '../../../../assets/Study'
 import * as s from './StudyOption.style'
-import {useRef} from "react";
-import {useMapping} from "../../../../components/common/MappingContext";
-import {HeartWhite} from "../../../../assets/FairyList";
+import { useRef } from 'react'
+import { useMapping } from '../../../../components/common/MappingContext'
 
-export default function StudyOption({nationSelect, setNationSelect, isLiked, setIsLiked, handleSearch}) {
-    const {countries} = useMapping();
-    const searchInputRef = useRef("");
+export default function StudyOption({
+  nationSelect,
+  setNationSelect,
+  isLiked,
+  setIsLiked,
+  handleSearch,
+}) {
+  const { countries } = useMapping()
+  const searchInputRef = useRef('')
 
-    return (
-        <s.OptionContainer>
-            {/* 선호 버튼 */}
-            <s.CircleBtn heart={isLiked} onClick={() => setIsLiked(!isLiked)}>
-                <img src={isLiked ? HeartIcon : HeartWhite} alt="heart"/>
-            </s.CircleBtn>
+  return (
+    <s.OptionContainer>
+      {/* 선호 버튼 */}
+      <s.CircleBtn heart={isLiked} onClick={() => setIsLiked(!isLiked)}>
+        <img src={isLiked ? HeartIcon : BigWhiteHeart} alt="heart" />
+      </s.CircleBtn>
 
-            {/* 국가 선택 */}
-            <s.NationSelectContainer>
-                {countries.map((nation, idx) => (
-                    <s.NationImg
-                        key={nation.id}
-                        src={`${process.env.REACT_APP_S3_BUCKET}${nation.flagUrl}`}
-                        alt={nation.name}
-                        check={nationSelect === nation.id}
-                        onClick={() => {
-                            const nationId = nation.id !== nationSelect ? nation.id : 0;
-                            setNationSelect(nationId)
-                        }}
-                    />
-                ))}
-            </s.NationSelectContainer>
+      {/* 국가 선택 */}
+      <s.NationSelectContainer>
+        {countries.map((nation, idx) => (
+          <s.NationImg
+            key={nation.id}
+            src={`${process.env.REACT_APP_S3_BUCKET}${nation.flagUrl}`}
+            alt={nation.name}
+            check={nationSelect === nation.id}
+            onClick={() => {
+              const nationId = nation.id !== nationSelect ? nation.id : 0
+              setNationSelect(nationId)
+            }}
+          />
+        ))}
+      </s.NationSelectContainer>
 
-            {/* 검색 버튼 */}
-            <s.CircleBtn search onClick={() => handleSearch(searchInputRef?.current?.value || "")}>
-                <img src={SearchIcon} alt="search"/>
-            </s.CircleBtn>
-        </s.OptionContainer>
-    )
+      {/* 검색 버튼 */}
+      <s.CircleBtn
+        search
+        onClick={() => handleSearch(searchInputRef?.current?.value || '')}
+      >
+        <img src={SearchIcon} alt="search" />
+      </s.CircleBtn>
+    </s.OptionContainer>
+  )
 }
